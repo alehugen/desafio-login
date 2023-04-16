428c816c56e6f7ed83eaef6fccaaf99866981375
Alessandro Hugen <alessandrohugen@gmail.com>
2023-04-16 20:47:43 -0300

    chore: set global css and font

--- src/App.vue
+++ src/App.vue
@@ -5,3 +5,24 @@
 </template>
 
 <script setup lang="ts"></script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  background-color: '#FAFAFC';
+  color: '#292D32';
+  -webkit-font-smoothing: 'antialiased';
+}
+
+body,
+input,
+textarea,
+button {
+  font-family: 'Roboto';
+  font-weight: 400;
+}
+</style>
