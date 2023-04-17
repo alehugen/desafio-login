ac79952c6c8bf24541a22e2550af8bedc342a6a3
Alessandro Hugen <alessandrohugen@gmail.com>
2023-04-16 22:47:26 -0300

    feat: create form structure

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="form-box">
+    <div class="form-titles">
+      <h2>Entre na sua conta</h2>
+      <h4>Para acessar sua conta informe seu e-mail e senha</h4>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+export default {}
+</script>
+<style lang="css">
+.form-box {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 2.75rem;
+  background-color: #ffffff;
+  border: 2px solid;
+  border-color: #e6eaf2;
+  border-radius: 5px;
+  max-width: 500px;
+  min-height: 400px;
+  color: '#2A343E';
+}
+
+.form-titles {
+  text-align: left;
+}
+</style>
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,10 +1,34 @@
 <template>
-  <div>
-    <h1>LOGIN heeelo</h1>
+  <div class="container">
+    <header class="header">
+      <Logo :image-height="48" :image-width="180" />
+    </header>
+    <LoginForm />
   </div>
 </template>
 <script lang="ts">
-export default {}
+import Logo from '../components/Logo.vue'
+import LoginForm from '../components/LoginForm.vue'
+
+export default {
+  components: {
+    Logo,
+    LoginForm
+  }
+}
 </script>
 
-<style></style>
+<style>
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.container header {
+  width: 100%;
+  margin-top: 5%;
+  padding: 1%;
+}
+</style>
