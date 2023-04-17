6f5a92f4538a752a740eb1afc9a0cbd93a45d879
Alessandro Hugen <alessandro.hugen@selectinvestimentos.com>
2023-04-17 09:59:00 -0300

    feat: add new routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,24 @@
-import { createRouter, createWebHistory} from "vue-router"
+import { createRouter, createWebHistory } from "vue-router"
 import Login from "../pages/Login.vue"
+import Home from "../pages/Home.vue"
+import Register from "../pages/Register.vue"
 
 const routes = [{
   path: "/",
   name: "login",
   component: Login,
-}]
+},
+{
+  path: "/home",
+  name: "Home",
+  component: Home,
+},
+{
+  path: "/register",
+  name: "Register",
+  component: Register,
+}
+]
 
 const router = createRouter({
   history: createWebHistory(),
