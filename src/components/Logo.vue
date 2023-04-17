0f1b25aba7ba844bf1590aa78e6f68ae0aff8932
Alessandro Hugen <alessandrohugen@gmail.com>
2023-04-16 22:28:00 -0300

    feat: implements Logo Component

---
+++ src/components/Logo.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="logo-box">
+    <img
+      src="../assets/locaweb.png"
+      :width="imageWidth"
+      :height="imageHeight"
+      alt="logotipo"
+    />
+  </div>
+</template>
+<script lang="ts">
+export default {
+  props: {
+    imageWidth: {
+      type: Number,
+      default: 100,
+      required: true
+    },
+    imageHeight: {
+      type: Number,
+      default: 100,
+      required: true
+    }
+  },
+  setup(props) {
+    const { imageHeight, imageWidth } = props
+    return {
+      imageHeight,
+      imageWidth
+    }
+  }
+}
+</script>
+<style lang="css">
+.logo-box {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 5px;
+}
+</style>
