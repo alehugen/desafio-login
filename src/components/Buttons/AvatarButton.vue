9e2102173ed5c3c424e3d777dc99af07baebc1a7
Alessandro Hugen <alessandro.hugen@selectinvestimentos.com>
2023-04-17 10:57:06 -0300

    feat: implement Avatar Component

---
+++ src/components/Buttons/AvatarButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="avatar">
+    <p>{{ user.charAt(0) }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    user: {
+      type: String,
+      required: true,
+    },
+  },
+  setup(props) {},
+};
+</script>
+
+<style>
+.avatar {
+  background-color: #d9d9d9;
+  width: 32px;
+  height: 32px;
+  border: 1px #d9d9d9 solid;
+  border-radius: 100%;
+  padding: 1rem;
+  margin-right: 1.75rem;
+  text-align: center;
+  color: #ffff;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
+    rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+  cursor: pointer;
+}
+</style>
