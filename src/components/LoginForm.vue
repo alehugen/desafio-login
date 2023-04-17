4dbb7c070536f297e7a568edc8eca9871195733e
Alessandro Hugen <alessandrohugen@gmail.com>
2023-04-17 00:34:13 -0300

    chore: add new font weight

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -4,10 +4,31 @@
       <h2>Entre na sua conta</h2>
       <h4>Para acessar sua conta informe seu e-mail e senha</h4>
     </div>
+
+    <TextInput
+      type="email"
+      placeholder="Seu e-mail"
+      :value="formInputs.user"
+      label="E-mail"
+    />
   </div>
 </template>
 <script lang="ts">
-export default {}
+import TextInput from './Inputs/TextInput.vue'
+
+export default {
+  components: { TextInput },
+  setup() {
+    const formInputs = {
+      user: '',
+      password: ''
+    }
+
+    return {
+      formInputs
+    }
+  }
+}
 </script>
 <style lang="css">
 .form-box {
@@ -18,7 +39,7 @@ export default {}
   padding: 2.75rem;
   background-color: #ffffff;
   border: 2px solid;
-  border-color: #e6eaf2;
+  border-color: #979899;
   border-radius: 5px;
   max-width: 500px;
   min-height: 400px;
