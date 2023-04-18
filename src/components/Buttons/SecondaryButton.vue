c676d97094785b53750bfb2f045fec72f7abec35
Alessandro Hugen <alessandro.hugen@selectinvestimentos.com>
2023-04-18 14:02:18 -0300

    feat: create SencondaryButton Component

--- src/components/Buttons/SecondaryButton.vue
+++ src/components/Buttons/SecondaryButton.vue
@@ -6,24 +6,24 @@ export default {
   props: {
     label: {
       type: String,
-      required: true
-    }
+      required: true,
+    },
   },
   setup(props) {
-    return {}
-  }
-}
+    return {};
+  },
+};
 </script>
 <style lang="css" scoped>
 button {
-  background-color: #f30168;
-  color: #ffff;
+  background-color: #ffffff;
+  color: #020202;
+  border: 1px black solid;
   width: 100%;
   border-radius: 5px;
-  border-style: none;
   padding: 1.7rem 0 1.7rem 0;
 
-  font-weight: 600;
+  font-weight: 400;
   font-size: large;
   text-transform: uppercase;
 
