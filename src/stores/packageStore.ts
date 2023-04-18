a08229cde6814cbeb21c9a1e295e8ad8a774d2f0
Alessandro Hugen <alessandro.hugen@selectinvestimentos.com>
2023-04-18 14:01:57 -0300

    fix: pinia store

--- src/stores/packageStore.ts
+++ src/stores/packageStore.ts
@@ -1,9 +1,9 @@
 import { defineStore } from 'pinia'
 import { PackageProps } from '../DTO/Package'
 
-export const usePackageStore = defineStore('pack', () => {
+export const usePackageStore = defineStore('Package', {
   // eslint-disable-next-line no-unused-labels
   state: () => {
-    return { pack: {} as PackageProps }
+    return { selected: {} as PackageProps }
   }
 })
