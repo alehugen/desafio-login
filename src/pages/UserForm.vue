c11ba11a8d4cae4ac2095d8ae7cfef5638a59022
Alessandro Hugen <alessandro.hugen@selectinvestimentos.com>
2023-04-18 14:03:17 -0300

    feat: implement user page

--- src/pages/UserForm.vue
+++ src/pages/UserForm.vue
@@ -1,19 +1,76 @@
 <template>
-  <h1>DADOS DO USUARIOS</h1>
+  <RegisterHeader />
+  <div class="grid-container">
+    <RegisterForm />
+    <PackageCard :packageInfo="selected" isCheckout>
+      <div class="blur"></div>
+      <SecondaryButton label="Trocar Plano" @click="changePackage()" />
+    </PackageCard>
+  </div>
 </template>
 
 <script lang="ts">
+import { onMounted } from "vue";
+import PackageCard from "../components/Register/PackageCard.vue";
+import RegisterForm from "../components/Register/RegisterForm.vue";
+import RegisterHeader from "../components/Register/RegisterHeader.vue";
 import { usePackageStore } from "../stores/packageStore";
+import router from "../routes";
+import SecondaryButton from "../components/Buttons/SecondaryButton.vue";
+
 
 export default {
+  components: { RegisterHeader, RegisterForm, PackageCard, SecondaryButton },
   setup() {
-    const store = usePackageStore();
+    const { selected } = usePackageStore();
+
+    function changePackage() {
+      router.back()
+    }
+
+    onMounted(() => {
+      if (!selected.id) {
+        router.push("/register");
+      }
+    });
 
     return {
-      store,
+      selected,
+      changePackage,
     };
   },
 };
 </script>
 
-<style></style>
+<style scoped>
+.grid-container {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  gap: 1.25rem;
+  padding: 0.5rem 0;
+  margin: 1.75rem 0;
+}
+
+.card {
+  width: 20%;
+  position: relative;
+}
+
+.blur {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(
+    0deg,
+    rgba(255, 255, 255, 1) 0%,
+    rgba(255, 255, 255, 0) 50%
+  );
+}
+
+button {
+  z-index: 1;
+}
+</style>
